57e2cfbfbfcf667b2aa3fed378ef93989b833da5
slightscan <tohohh1@gmail.com>
2023-09-18 06:34:20 +0700

    commit

--- src/hats/hats-parser.test.ts
+++ src/hats/hats-parser.test.ts
@@ -28,7 +28,7 @@ it("parser contest", async () => {
   expect(contest.pk).toBe("2023-08-stakewise")
 
   expect(contest.prize).toBe("150000")
-  expect(contest.modules.length).toBe(0)
+  expect(contest.modules.length).toBe(40)
   expect(contest.url).toBe("https://app.hats.finance/audit-competitions/stakewise-0xd91cd6ed6c9a112fdc112b1a3c66e47697f522cd")
   expect(contest.repo_urls![0]).toBe("https://github.com/stakewise/v3-core")
   expect(contest.doc_urls![0]).toBe("https://docs-v3.stakewise.io")
