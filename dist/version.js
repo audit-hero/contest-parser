062917e067f664d6974337cfac891f03e3473851
totovoto <tohohh1@gmail.com>
2024-07-18 07:38:07 +0300

    update update

--- dist/version.js
+++ dist/version.js
@@ -1,4 +1,3 @@
-// update with ./scripts/deploy.sh. note this version needs to be tha same as in package.json
 export let version = "0.0.4";
 console.log(`contest-parser version: ${version}`);
 //# sourceMappingURL=version.js.map
--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,2 @@
-// update with ./scripts/deploy.sh. note this version needs to be tha same as in package.json
 export let version = "0.0.4"
 console.log(`contest-parser version: ${version}`)
