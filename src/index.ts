b9512e4eec9bfbd1c94be7ea2357032e74a0594b
totovoto <tohohh1@gmail.com>
2024-11-22 20:08:53 +0700

    update update

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./c4/c4ContestParser.js"
 export * from "./c4/getActiveC4Contests.js"
 
 export * from "./codehawks/codehawksParser.js"
+export * from "./hackenproof/hackenParser.js"
 export * from "./hats/hats-parser.js"
 export * from "./sherlock/sherlockContestParser.js"
 export { setLogLevel } from "./config.js"
